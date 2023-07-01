ea97768ee46e62650b1ba6ce8722f2ca12ef548d
Job Cabral <jcabral@engagednation.com>
2023-07-01 02:18:04 -0700

    Added lvim configurations.

---
+++ lvim/config.lua
@@ -0,0 +1,39 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
+lvim.plugins = {
+  {
+    "Pocco81/auto-save.nvim",
+    config = function()
+      require("auto-save").setup()
+    end,
+  },
+  {
+    "kevinhwang91/nvim-bqf",
+    event = { "BufRead", "BufNew" },
+    config = function()
+    require("bqf").setup({
+            auto_enable = true,
+            preview = {
+            win_height = 12,
+            win_vheight = 12,
+            delay_syntax = 80,
+            border_chars = { "┃", "┃", "━", "━", "┏", "┓", "┗", "┛", "█" },
+            },
+            func_map = {
+            vsplit = "",
+            ptogglemode = "z,",
+            stoggleup = "",
+            },
+            filter = {
+            fzf = {
+            action_for = { ["ctrl-s"] = "split" },
+            extra_opts = { "--bind", "ctrl-o:toggle-all", "--prompt", "> " },
+            },
+            },
+            })
+    end,
+  },
+}
