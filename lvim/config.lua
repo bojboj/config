c01630c0c1213c03b39c10b0646375fa3fb7c15c
Job Cabral <mr.bojboj@icloud.com>
2023-07-26 01:17:12 -0700

    Updated kitty and lvim config.

--- lvim/config.lua
+++ lvim/config.lua
@@ -2,6 +2,9 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
+--
+
+lvim.transparent_window = true
 
 lvim.keys.normal_mode["*"] = "*``"
 lvim.keys.normal_mode["#"] = "#``"
