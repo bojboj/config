8d05a9220f8c0e8f011b5c80fc198508580bda02
Job Cabral <mr.bojboj@icloud.com>
2024-07-12 22:39:48 -0700

    Updated tmux and lazyvim config.

--- lazyvim/lua/plugins/lualine.lua
+++ lazyvim/lua/plugins/lualine.lua
@@ -3,6 +3,8 @@ return {
     "nvim-lualine/lualine.nvim",
     opts = function(_, opts)
       opts.sections.lualine_z = {}
+      opts.options.section_separators = ""
+      opts.options.component_separators = ""
     end,
   },
 }
