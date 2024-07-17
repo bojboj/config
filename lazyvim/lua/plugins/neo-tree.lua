2619580cc71a71dd10a0fda6b5cab15dff3add1f
Job Cabral <mr.bojboj@icloud.com>
2024-07-17 01:39:13 -0700

    Updated neovim and tmux config.

--- lazyvim/lua/plugins/neo-tree.lua
+++ lazyvim/lua/plugins/neo-tree.lua
@@ -4,7 +4,14 @@ return {
     opts = {
       filesystem = {
         filtered_items = {
-          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_by_name = {
+            ".DS_Store",
+            "thumbs.db",
+            ".git",
+            ".idea",
+          },
         },
       },
     },
