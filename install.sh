1df88ce0a8d6ea4b06df08f53db2171bc504e8ee
Job Cabral <mr.bojboj@icloud.com>
2024-08-19 02:31:40 -0700

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-myconfigDir=$(pwd)
+DOTFILES=$(pwd)
 
-cd ~
-ln -sf "$myconfigDir/zshrc" .zshrc
-ln -sf "$myconfigDir/editorconfig" .editorconfig
-ln -sf "$myconfigDir/tmux.conf" .tmux.conf
+cd rm "$HOME/.zshrc"
+ln -s "$DOTFILES/zshrc" "$HOME/.zshrc"
+
+rm "$HOME/.editorconfig"
+ln -s "$DOTFILES/editorconfig" "$HOME/.editorconfig"
+
+rm "$HOME/.tmux.conf"
+ln -s "$DOTFILES/tmux.conf" "$HOME/.tmux.conf"
+
+rm "$HOME/.config/helix"
+ln -s "$DOTFILES/helix" "$HOME/.config/helix"
