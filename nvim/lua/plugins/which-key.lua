08a867db522a8c8a1d29ec9a24b3771317134101
Job Cabral <jcabral@engagednation.com>
2024-08-20 11:08:51 -0700

    Update keymappings.

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -5,12 +5,12 @@ return {
   opts = {
     delay = 800,
     spec = {
+      {"<leader>\\", "<cmd>noh<cr>", desc = "Clear Search Highlight", mode = "n"},
       {"<leader>b", "<cmd>Neotree buffers float<cr>", desc = "Buffer Select", mode = "n"},
       {"<leader>e", "<cmd>Neotree reveal<cr>", desc = "File Explorer", mode = "n"},
       {"<leader>f", "<cmd>Telescope find_files<cr>", desc = "Find File", mode = "n"},
       {"<leader>g", "<cmd>Neotree git_status reveal<cr>", desc = "Git Status Explorer", mode = "n"},
       {"<leader>s", "<cmd>Telescope live_grep<cr>", desc = "Search Text", mode = "n"},
-      {"<leader>x", "<cmd>noh<cr>", desc = "Clear Search Highlight", mode = "n"},
     }
   },
 }
