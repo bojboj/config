e2e7bd62b5c0e26dac542a3bdabc5609e43f9e12
Job Cabral <mr.bojboj@icloud.com>
2024-09-03 08:04:34 +0000

    Added gitconfig

--- install.sh
+++ install.sh
@@ -13,9 +13,10 @@ if [ ! -d ~/.fzf ]; then
     ~/.fzf/install --all
 fi
 
-ln -fs $DOTFILES/zshrc $HOME/.zshrc
 ln -fs $DOTFILES/editorconfig $HOME/.editorconfig
-ln -fs $DOTFILES/tmux.conf $HOME/.tmux.conf
+ln -fs $DOTFILES/gitconfig $HOME/.gitconfig
 ln -fs $DOTFILES/ideavimrc $HOME/.ideavimrc
-ln -fs $DOTFILES/vscode/settings.json $HOME/.local/share/code-server/Machine/settings.json
 ln -fs $DOTFILES/nvim $HOME/.config/
+ln -fs $DOTFILES/tmux.conf $HOME/.tmux.conf
+ln -fs $DOTFILES/vscode/settings.json $HOME/.local/share/code-server/Machine/settings.json
+ln -fs $DOTFILES/zshrc $HOME/.zshrc
